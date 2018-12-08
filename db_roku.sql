7af471679d5472a13ad6ffefdecdcb1585fe5af4
Patel <j_patel73637@tss-hr420-dm37.fc.ca>
2018-12-08 17:02:33 -0500

    working files

--- db_roku.sql
+++ db_roku.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 27, 2018 at 12:05 AM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Host: localhost:8889
+-- Generation Time: Dec 08, 2018 at 10:00 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,11 +26,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_home`
 --
 
-DROP TABLE IF EXISTS `tbl_home`;
-CREATE TABLE IF NOT EXISTS `tbl_home` (
+CREATE TABLE `tbl_home` (
   `username` varchar(15) NOT NULL,
-  `password` varchar(15) NOT NULL,
-  PRIMARY KEY (`username`)
+  `password` varchar(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -41,14 +37,17 @@ CREATE TABLE IF NOT EXISTS `tbl_home` (
 -- Table structure for table `tbl_kids`
 --
 
-DROP TABLE IF EXISTS `tbl_kids`;
-CREATE TABLE IF NOT EXISTS `tbl_kids` (
-  `kids_id` int(5) NOT NULL AUTO_INCREMENT,
-  `kids_education_channels` varchar(50) NOT NULL,
-  `kids_entertainment` varchar(50) NOT NULL,
-  `kids_activity_channels` varchar(50) NOT NULL,
-  PRIMARY KEY (`kids_id`)
+CREATE TABLE `tbl_kids` (
+  `kids_id` int(5) NOT NULL,
+  `kids_gallery` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kids`
+--
+
+INSERT INTO `tbl_kids` (`kids_id`, `kids_gallery`) VALUES
+(1, 'kids_roku.jpg');
 
 -- --------------------------------------------------------
 
@@ -56,15 +55,50 @@ CREATE TABLE IF NOT EXISTS `tbl_kids` (
 -- Table structure for table `tbl_parents`
 --
 
-DROP TABLE IF EXISTS `tbl_parents`;
-CREATE TABLE IF NOT EXISTS `tbl_parents` (
-  `parents_id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_parents` (
+  `parents_id` int(5) NOT NULL,
   `password` varchar(15) NOT NULL,
   `add_channel` varchar(50) NOT NULL,
-  `purchase_channel` varchar(50) NOT NULL,
-  PRIMARY KEY (`parents_id`)
+  `purchase_channel` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_home`
+--
+ALTER TABLE `tbl_home`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `tbl_kids`
+--
+ALTER TABLE `tbl_kids`
+  ADD PRIMARY KEY (`kids_id`);
+
+--
+-- Indexes for table `tbl_parents`
+--
+ALTER TABLE `tbl_parents`
+  ADD PRIMARY KEY (`parents_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_kids`
+--
+ALTER TABLE `tbl_kids`
+  MODIFY `kids_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_parents`
+--
+ALTER TABLE `tbl_parents`
+  MODIFY `parents_id` int(5) NOT NULL AUTO_INCREMENT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
     $user = "root";
-    $pw = "";
+    $pw = "root";
 
     try {
         $conn = new PDO('mysql:host=localhost;dbname=db_roku', $user, $pw);
